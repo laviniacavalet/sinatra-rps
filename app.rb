04754a639018d23349f142f8e9367fb1b4e8ace3
laviniacavalet <116130863+laviniacavalet@users.noreply.github.com>
2024-04-09 19:52:23 +0000

    addede rock paper scissors routes

--- app.rb
+++ app.rb
@@ -7,3 +7,48 @@ get("/") do
   <p>Define some routes in app.rb</p>
   "
 end
+
+get("/rock") do
+  moves = ["rock", "paper", "scissors"]
+
+  @comp_moves = moves.sample
+
+  if @comp_moves == "rock"
+    @outcome = "tied"
+  elsif @comp_moves == "paper"
+    @outcome = "lost"
+  else
+    @outcome = "won"
+  end
+  erb(:rock)
+end
+
+get("/paper") do
+  moves = ["rock", "paper", "scissors"]
+  
+  @comp_moves = moves.sample
+
+  if @comp_moves == "paper"
+    @outcome = "tied"
+  elsif @comp_moves == "scissors"
+    @outcome = "lost"
+  else
+    @outcome = "won"
+  end
+  erb(:paper)
+end
+
+get("/scissors") do
+  moves = ["rock", "paper", "scissors"]
+  
+  @comp_moves = moves.sample
+
+  if @comp_moves == "scissors"
+    @outcome = "tied"
+  elsif @comp_moves == "rock"
+    @outcome = "lost"
+  else
+    @outcome = "won"
+  end
+  erb(:scissors)
+end
